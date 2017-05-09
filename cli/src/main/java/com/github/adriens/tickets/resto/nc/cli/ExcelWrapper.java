09144d0f6d836f22debebb698bf6cc3661c494df
salad74 <salad74@MI3201.site-mairie.noumea.nc>
2017-05-09 17:56:57 +1100

    First dirty code that creates/updates the excel of the Solde.

--- cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/ExcelWrapper.java
+++ cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/ExcelWrapper.java
@@ -8,17 +8,24 @@ package com.github.adriens.tickets.resto.nc.cli;
 import com.github.adriens.tickets.resto.nc.api.Affilie;
 import com.github.adriens.tickets.resto.nc.api.TicketsRestaurantsServiceWrapper;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import javafx.scene.transform.Affine;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -27,83 +34,134 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author salad74
  */
 public class ExcelWrapper {
-    
+
     private XSSFWorkbook workbook;
-    
+
     private XSSFSheet affiliesSheet;
-    
+
     /*
     public ExcelWrapper(){
         
     }*/
-    
-    public ExcelWrapper(){
+    public ExcelWrapper() {
         //this.fileName = fileName;
         this.workbook = new XSSFWorkbook();
-        
-        
+
     }
-    
-    public void create(String fileName) throws FileNotFoundException, IOException, Exception{
-            generateAffiliesSheet();
-            FileOutputStream outputStream = new FileOutputStream(fileName);
-            workbook.write(outputStream);
-            workbook.close();
+
+    public void create(String fileName) throws FileNotFoundException, IOException, Exception {
+        generateAffiliesSheet();
+        FileOutputStream outputStream = new FileOutputStream(fileName);
+        workbook.write(outputStream);
+        workbook.close();
     }
-    
-    
-    
-    public  void generateAffiliesSheet() throws IOException, Exception{
-         affiliesSheet = workbook.createSheet("Affilies");
-         /*Object[][] datatypes = {
+
+    public void createOrUpdateSolde(String filename, String login, String password) throws IOException, InvalidFormatException {
+
+        // load workbook
+        File inputSoldeFile = new File(filename);
+        XSSFWorkbook soldeWorkbook;
+        XSSFSheet soldeSheet;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        if (!inputSoldeFile.exists()) {
+
+            soldeWorkbook = new XSSFWorkbook();
+            FileOutputStream tmpOut = new FileOutputStream("solde.xlsx");
+            soldeWorkbook.createSheet("Solde");
+            soldeWorkbook.write(tmpOut);
+            tmpOut.flush();
+            tmpOut.close();
+        }
+        // here file exists
+        FileInputStream fileIn = new FileInputStream(new File("solde.xlsx"));
+        soldeWorkbook = new XSSFWorkbook(fileIn);
+        soldeSheet = soldeWorkbook.getSheet("Solde");
+        int lastRowNum = soldeSheet.getPhysicalNumberOfRows();
+        System.out.println("nb rows found : " + lastRowNum);
+        if (lastRowNum == 0) {
+            Row soldeRow = soldeSheet.createRow(0);
+            Cell soldeCell = soldeRow.createCell(0);
+            soldeCell.setCellValue("Solde");
+
+            Cell updateCell = soldeRow.createCell(1);
+            updateCell.setCellValue("Update Date");
+        } else {
+            // shift down rows from 1
+            soldeSheet.shiftRows(1, soldeSheet.getLastRowNum(), 1);
+        }
+
+        Row soldeRow = soldeSheet.createRow(1);
+
+        Cell soldeCell = soldeRow.createCell(0);
+        // get cuurent online solde
+        Tic
+        soldeCell.setCellValue(21310);
+
+        Cell updateCell = soldeRow.createCell(1);
+        updateCell.setCellValue(sdf.format(new Date()));
+
+        soldeSheet.autoSizeColumn(0);
+        soldeSheet.autoSizeColumn(1);
+        fileIn.close();
+
+        FileOutputStream fileOut = new FileOutputStream("solde.xlsx");
+        soldeWorkbook.write(fileOut);
+        fileOut.flush();
+        fileOut.close();
+    }
+
+    public void generateAffiliesSheet() throws IOException, Exception {
+        affiliesSheet = workbook.createSheet("Affilies");
+        /*Object[][] datatypes = {
                 {"Enseigne", "Catégorie", "Cuisine", "Téléphone", "Adresse", "Commune", "Quartier"}
         };
-        */
-         ArrayList<Affilie> affList = TicketsRestaurantsServiceWrapper.getAffilies();
-         int rowNum = 0;
-         Iterator<Affilie> affIter = affList.iterator();
-         Affilie lAff;
-         String gmapsUrl = "";
-         FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
-         while(affIter.hasNext()){
-             Row row = affiliesSheet.createRow(rowNum++);
-             lAff = affIter.next();
-             
-             Cell cellEnseigne = row.createCell(0);
-             cellEnseigne.setCellValue(lAff.getEnseigne());
-             
-             Cell cellCateg = row.createCell(1);
-             cellCateg.setCellValue(URLDecoder.decode(lAff.getCategorie(), "UTF-8"));
-             
-             Cell cellCuisi = row.createCell(2);
-             cellCuisi.setCellValue(lAff.getCuisine());
-             
-             Cell cellTel = row.createCell(3);
-             cellTel.setCellValue(Affilie.getFormattedTelephoneNumber(lAff.getTelephone()));
-             
-             Cell cellAdr = row.createCell(4);
-             cellAdr.setCellValue(lAff.getAdresse());
-             
-             Cell cellComm = row.createCell(5);
-             cellComm.setCellValue(lAff.getCommune());
-             
-             Cell cellQuar = row.createCell(6);
-             cellQuar.setCellValue(lAff.getQuartier());
-             
-             Cell cellFullAdress = row.createCell(7);
-             gmapsUrl = "http://maps.google.com/maps?q=" + lAff.getFormattedAdress();
-             cellFullAdress.setCellFormula("HYPERLINK(\"" + gmapsUrl+ "\",\"Voir sur Google Maps\")");
-             
-    
-         }
+         */
+        ArrayList<Affilie> affList = TicketsRestaurantsServiceWrapper.getAffilies();
+        int rowNum = 0;
+        Iterator<Affilie> affIter = affList.iterator();
+        Affilie lAff;
+        String gmapsUrl = "";
+        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+        while (affIter.hasNext()) {
+            Row row = affiliesSheet.createRow(rowNum++);
+            lAff = affIter.next();
 
-    affiliesSheet.autoSizeColumn(0);
-    affiliesSheet.autoSizeColumn(1);
-    affiliesSheet.autoSizeColumn(2);
-    affiliesSheet.autoSizeColumn(3);
-    affiliesSheet.autoSizeColumn(4);
-    affiliesSheet.autoSizeColumn(5);
-    affiliesSheet.autoSizeColumn(6);
-    affiliesSheet.autoSizeColumn(7);
+            Cell cellEnseigne = row.createCell(0);
+            cellEnseigne.setCellValue(lAff.getEnseigne());
+
+            Cell cellCateg = row.createCell(1);
+            cellCateg.setCellValue(URLDecoder.decode(lAff.getCategorie(), "UTF-8"));
+
+            Cell cellCuisi = row.createCell(2);
+            cellCuisi.setCellValue(lAff.getCuisine());
+
+            Cell cellTel = row.createCell(3);
+            cellTel.setCellValue(Affilie.getFormattedTelephoneNumber(lAff.getTelephone()));
+
+            Cell cellAdr = row.createCell(4);
+            cellAdr.setCellValue(lAff.getAdresse());
+
+            Cell cellComm = row.createCell(5);
+            cellComm.setCellValue(lAff.getCommune());
+
+            Cell cellQuar = row.createCell(6);
+            cellQuar.setCellValue(lAff.getQuartier());
+
+            Cell cellFullAdress = row.createCell(7);
+            gmapsUrl = "http://maps.google.com/maps?q=" + lAff.getFormattedAdress();
+            cellFullAdress.setCellFormula("HYPERLINK(\"" + gmapsUrl + "\",\"Voir sur Google Maps\")");
+
+        }
+
+        affiliesSheet.autoSizeColumn(0);
+        affiliesSheet.autoSizeColumn(1);
+        affiliesSheet.autoSizeColumn(2);
+        affiliesSheet.autoSizeColumn(3);
+        affiliesSheet.autoSizeColumn(4);
+        affiliesSheet.autoSizeColumn(5);
+        affiliesSheet.autoSizeColumn(6);
+        affiliesSheet.autoSizeColumn(7);
     }
 }
--- cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/Main.java
+++ cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/Main.java
@@ -13,7 +13,9 @@ public class Main {
        public static void main(String[] args) {
             try{
                 ExcelWrapper wrap = new ExcelWrapper();
-            wrap.create("affilies.xlsx");
+            //wrap.create("affilies.xlsx");
+            
+            wrap.createOrUpdateSolde("solde.xlsx", args[0], args[1]);
             }
             catch(Exception ex){
                 ex.printStackTrace();
