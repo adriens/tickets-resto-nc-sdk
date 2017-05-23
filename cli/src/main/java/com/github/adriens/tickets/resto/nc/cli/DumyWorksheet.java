3d9ba9dd50fe286c340c6a9c89497c9dcd8941e5
adriens <Adrien.Sales@gmail.com>
2017-05-23 11:05:23 +1100

    Remove dirty sysouts #24

--- cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/DumyWorksheet.java
+++ cli/src/main/java/com/github/adriens/tickets/resto/nc/cli/DumyWorksheet.java
@@ -5,11 +5,9 @@
  */
 package com.github.adriens.tickets.resto.nc.cli;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
